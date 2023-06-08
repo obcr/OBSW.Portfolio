6bc23f4814d15dd2504ce9ddaa11ddd19f4f4dbb
OliB <obianciotto@samfex.com>
2023-06-08 10:07:59 +0100

    css behaviour test

--- js/app.js
+++ js/app.js
@@ -1,2 +1,9 @@
 console.log("hello world");
 
+// Désactiver le rechargement de la page lors du défilement vers le bas sur mobile
+document.addEventListener('touchmove', function(event) {
+    // Vérifier si l'utilisateur fait défiler vers le bas
+    if (event.touches[0].clientY < 0) {
+      event.preventDefault(); // Empêcher le comportement par défaut du défilement
+    }
+  }, { passive: false });
